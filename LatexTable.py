2b18e739fdafa154a9a1023a9ab7a7208d9930be
safwankdb <kdbeatbox@gmail.com>
2018-09-03 02:59:20 +0530

    Initial Commit

---
+++ LatexTable.py
@@ -0,0 +1,11 @@
+import pyperclip as pc
+data = [ i.split('\t') for i in pc.paste().split('\n')]
+header = "\\begin{table}[]\n\\centering\n\\begin{tabular}{|" + 'l|'*len(data[0]) + "}\n\\hline\n"
+footer = "\\end{tabular}\n\\caption{My caption}\n\\label{my-label}\n\\end{table}"
+latex_code = header
+for values in data:
+	for value in values:
+		latex_code += value + " & "
+	latex_code = latex_code[:-3] + " \\\\ \\hline\n"
+latex_code += footer 
+print(latex_code)
